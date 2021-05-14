6671c7d35f1db8083f4dca3c24126020634f1d21
Simu1325 <82212011+Simu1325@users.noreply.github.com>
2021-05-14 13:15:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,40 @@
-
 var cat,cat_walking,cat_sitting,cat_collided,catImage;
 var mouse,mouse_standing,mouse_teasing,mouse_collided,mouseImage;
 var garden, gardenImage;
 
 
 function preload(){
-  cat_sitting = loadImage("images/cat1.png");
-  cat_walking = loadAnimation("images/cat2.png","images/cat3.png");
-  cat_collided = loadImage("images/cat4.png");
+  cat_sitting = loadImage("cat1.png");
+  cat_walking = loadAnimation("cat2.png,cat3.png");
+  cat_collided = loadImage("cat4.png");
   
-  gardenImage = loadImage("images/garden.png");
+  gardenImage = loadImage("garden.png");
 
-  mouse_teasing = loadAnimation("images/mouse1.png","images/mouse3.png");
-  mouse_standing = loadImage("images/mouse2.png");
-  mouse_collided = loadImage("images/mouse4.png");
+  mouse_teasing = loadAnimation("mouse1.png,mouse3.png");
+  mouse_standing = loadImage("mouse2.png");
+  mouse_collided = loadImage("mouse4.png");
   }
 
 function setup() {
   createCanvas(1000,800);
 
-  cat = createSprite(800,600,20,50);
+  cat = createSprite(50,160,20,50);
   cat.addImage("sitting",cat_sitting);
   cat.addAnimation("walking", cat_walking);
   cat.addImage("collided", cat_collided);
   
-  cat.scale = 0.2;
+  cat.scale = 0.5;
   
-  mouse = createSprite(200,600,20,50);
+  mouse = createSprite(50,160,20,50);
   mouse.addImage("standing",mouse_standing);
   mouse.addAnimation("teasing", mouse_teasing);
   mouse.addImage("collided", mouse_collided);
   
-  mouse.scale = 0.15;
+  mouse.scale = 0.2;
+
+  garden = createSprite(1000,800,50,50);
+  garden.addImage(" garden", gardenImage);
+  garden.x =  garden.width /2;
   
   cat.setCollider("rectangle",0,0,cat.width,cat.height);
   cat.debug = true
@@ -43,7 +46,7 @@ function setup() {
 
 function draw() {
   
-  background(gardenImage);
+  background(225);
 
     if(cat.x - mouse.x< (cat.width - mouse.width)/2){
     cat.changeAnimation("collided",cat_collided);
@@ -57,16 +60,16 @@ function draw() {
   drawSprites();
 }
   function keyPressed(){
-
     if(keyCode === RIGHT_ARROW){
-      mouse.frameDelay = 25;
-      mouse.x = mouse.x+10
-      mouse.addAnimation("mouse_teasing",mouseImage);
-      mouse.changeAnimation("mouse_teasing");
-       }
-
-   if(keyCode === LEFT_ARROW){
-    cat.frameDelay = 25;
-    cat.x = cat.x-10
+        mouse.addAnimation("mouse_teasing",mouse1.png,);
+        mouse.changeAnimation("mouse_teasing");
+        mouse.frameDelay = 25;
     }
-}
+ 
+    if(keyCode === LEFT_ARROW){
+     mouse.addAnimation("mouse_teasing",mouse3.png,);
+     mouse.changeAnimation("mouse_teasing");
+     mouse.frameDelay = 25;
+ }
+ 
+   }
